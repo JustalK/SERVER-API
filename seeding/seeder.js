adf7c6c2e99ecb9adea89bb951f40b35e0acaa3c
Latsuj <justal.kevin@gmail.com>
2021-01-18 18:54:15 +0800

    [FEATURE] Adding the alias on seeding

--- seeding/seeder.js
+++ seeding/seeder.js
@@ -1,40 +1,41 @@
-'use strict';
+'use strict'
 
-const utils = require('../src/libs/utils');
-const mode = utils.mode(process.env.NODE_ENV);
-require('dotenv').config({path: './env/.env.' + mode});
-const path = require('path');
-const { Seeder } = require('mongo-seeding');
+require('module-alias/register')
+const utils = require('@src/libs/utils')
+const mode = utils.mode(process.env.NODE_ENV)
+require('dotenv').config({ path: './env/.env.' + mode })
+const path = require('path')
+const { Seeder } = require('mongo-seeding')
 
 module.exports = {
-	/**
-	* Return the created seeder with the config given
-	* @params {Object} config The config for the seeder
-	* @return {Seeder} The seeder object
-	**/
-	get_seeder: (config) => {
-		return new Seeder(config);
-	},
-	/**
-	* Seed the database with the informations in data
-	**/
-	seed: async () => {
-		const seeder = module.exports.get_seeder({
-			database: process.env.DB_URI_DATA + process.env.DB_NAME,
-			dropDatabase: true,
-		})
+  /**
+  * Return the created seeder with the config given
+  * @params {Object} config The config for the seeder
+  * @return {Seeder} The seeder object
+  **/
+  get_seeder: (config) => {
+    return new Seeder(config)
+  },
+  /**
+  * Seed the database with the informations in data
+  **/
+  seed: async () => {
+    const seeder = module.exports.get_seeder({
+      database: process.env.DB_URI_DATA + process.env.DB_NAME,
+      dropDatabase: true
+    })
 
-		const collectionReadingOptions = {
-		    transformers: [
-		        Seeder.Transformers.replaceDocumentIdWithUnderscoreId,
-		    ]
-		}
+    const collectionReadingOptions = {
+      transformers: [
+        Seeder.Transformers.replaceDocumentIdWithUnderscoreId
+      ]
+    }
 
-		const collections = seeder.readCollectionsFromPath(
-		    path.resolve("./seeding/datas"),
-		    collectionReadingOptions
-		);
+    const collections = seeder.readCollectionsFromPath(
+      path.resolve('./seeding/datas'),
+      collectionReadingOptions
+    )
 
-		await seeder.import(collections);
-	}
+    await seeder.import(collections)
+  }
 }
