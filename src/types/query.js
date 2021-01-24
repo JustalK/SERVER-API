2f4ecb66c7bc68b6e90a8a54fc3b69d15b8bfe54
Latsuj <justal.kevin@gmail.com>
2021-01-24 17:35:44 +0800

    [DOCUMENTATION] Adding the first documentation in the graph

--- src/types/query.js
+++ src/types/query.js
@@ -5,7 +5,11 @@ const { gql } = require('apollo-server-fastify')
 module.exports = gql`
   # Root Query
   type Query  {
-    # Get all the user, can only be use if logged
+    """
+    Return:
+    true : DB save successful
+    false : DB save unsuccessful
+    """
     get_all_users: [User]! @isLoggedIn
 
     # Get the config of the API
